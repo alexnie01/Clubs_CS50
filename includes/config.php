9661a190906134e033e768bbd83272a8a3f3b8a8
Alex Nie <anie@college.harvard.edu>
2014-12-06 00:39:10 -0500

    added universal access

--- includes/config.php
+++ includes/config.php
@@ -33,7 +33,7 @@
     session_start();
 
     // require authentication for all pages except /login.php, /logout.php, and /register.php
-    if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/logout.php", "/register.php"]))
+    if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/logout.php", "/register.php", "/mainController.php"]))
     {
         if (empty($_SESSION["id"]))
         {
