1cd55a399967ac52d1236dd3009dc872ca7907d6
Alex Nie <anie@college.harvard.edu>
2014-12-06 02:44:46 -0500

    changed login.php to login2.php and templogin.php to login.php

---
+++ public/login2.php
@@ -0,0 +1,51 @@
+
+<?php
+    
+    // import SQL query functionality
+    require("../includes/search.php");
+
+    // if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("templogin_form.php", ["title" => "Log In"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // validate submission
+        if (empty($_POST["username"]))
+        {
+            apologize("You must provide your username.");
+        }
+        else if (empty($_POST["password"]))
+        {
+            apologize("You must provide your password.");
+        }
+
+        // query database for user
+        $rows = lookup_user($_POST["username"], $_POST["password"]);
+
+        // if we found user, check password
+        if (count($rows) == 1)
+        {
+            // first (and only) row
+            $row = $rows[0];
+
+            // compare hash of user's input against hash that's in database
+            if ($_POST["password"] == $row["password"])
+            {
+                // remember that user's now logged in by storing user's ID in session
+                $_SESSION["id"] = $row["user_id"];
+
+                // redirect
+                redirect("mainController.php");
+            }
+        }
+
+        // else apologize
+        apologize("Invalid username and/or password.");
+    }
+
+?>
