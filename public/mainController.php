9e9478db91e22cb567ada6740af58354eed22173
Alex Nie <anie@college.harvard.edu>
2014-12-06 07:11:06 -0500

    commented out javascript

--- public/mainController.php
+++ public/mainController.php
@@ -7,10 +7,10 @@
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
         // performs empty search that retrieves all clubs
-        $search_results = search("", 0, 0, true, 0, 1000, 0, '1000-01-01',[""]); 
+        $results = search("", 0, 0, true, 0, 1000, 0, '1000-01-01',[""]); 
         
         // renders first page
-        render("testsearch0.html", ["search_results" => $search_results]);
+        render("testsearch0.html", ["search_results" => $results[0], "info" => $results[1]]);
         print("GET request received");
 
     }
@@ -20,14 +20,14 @@
     {
         print("POST request received");
         // user submitted universal search
-        $search_results = universal_search();
+        $results = universal_search();
         
         // retrieve function: given a $table variable and $user_id, retrieve from this $table
         
         // deletion funtion
         
         //return to mainTemplate page
-        render("testsearch0.html", ["search_results" => $search_results]);
+        render("testsearch0.html", ["search_results" => $results[0], "info" => $results[1]]);
         
     }
        
